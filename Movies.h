e3c8360a58ba14a14376b29ca1b6e525f7722e31
XPS-13 <chen.k.yam@gmail.com>
2019-10-31 23:58:55 -0400

    added Movies class which encapsulates a vector

---
+++ Movies.h
@@ -0,0 +1,15 @@
+#ifndef MOVIES_H
+#define MOVIES_H
+#include <vector>
+#include "Movie.h"
+
+
+class Movies {
+	std::vector<Movie> collection;
+public:
+	Movies();
+	~Movies();
+
+};
+
+#endif // MOVIES_H
